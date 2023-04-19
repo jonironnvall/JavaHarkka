aec450aa1b601705966d78bcf73cafd60ef7d46f
LeeviOva <Leevi.ovaskainen@edu.turkuamk.fi>
2023-04-19 19:47:39 +0300

    Patch 201.23-4c
    Most of the changes posted below. (Forgor saving : / )

--- sudokuLogic/gameGenerator.java
+++ sudokuLogic/gameGenerator.java
@@ -1,27 +1,53 @@
 package sudokuLogic;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
+import sudokuLogic.Coordinates;
+import sudokuLogic.Rows;
 
 
 
 public class gameGenerator {
+
+
+
+    public static void main (String[]args){
+    solvedGameGeneration();
+    System.out.println("Ohjelma on päättynyt");
+
+
+    }
+
 
     public static int[][] solvedGameGeneration() {
         Random random = new Random(System.currentTimeMillis());     /* Creating a random number generator */
         int[][] newGrid = new int[9][9];                            /* NOTE: this part is now usable for only 9 x 9 grids, smaller ones are off the table with this */
         
         for (int value = 1; value <= 9; value++){                   /* Outer for loop for making sure dublicate numbers in the same positions wont occur. Tests all positions */
-            int set = 0;
+            int allocations = 0;
             int interrupt = 0;
             int tries = 0;
+            List<Coordinates> allocTracker = new ArrayList<>();
+
             tries++;
 
-            while (set < 9){
-                if(tries > 500){
-                    nukeCurrentGame(newGrid);
-                    interrupt = 0;
-                    value = 1;
+            while (allocations < 9){
+                if(interrupt > 200){
+                    allocTracker.forEach(coord -> {
+                        newGrid[coord.getX()][coord.getY()] = 0;   });
+                        
+                        allocations = 0;
+                        interrupt = 0;
+                        allocTracker.clear();
+                         tries++;
+                
+                        if (tries > 500) {
+                            nukeCurrentGame(newGrid);
+                            tries = 0;
+                            value = 1;
                 }
             }
             int xCoordinate = random.nextInt(9);
@@ -30,15 +56,21 @@ public class gameGenerator {
             if(newGrid[xCoordinate][yCoordinate] == 0){
                 newGrid[xCoordinate][yCoordinate] = value;
                 
-                if(gameSolver.invalidSudoku(newGrid)) {
-                    newGrid[xCoordinate][yCoordinate]= 0;
-                    value--;                                           /* Experimental - needs some way to back track and try again, this aint it chief */
-                    interrupt++;                                
-                }
+                if(invalidsudoku(newGrid)) {
+                    newGrid[xCoordinate][yCoordinate] = 0;
+                    interrupt++;
             }
-
+            else {
+                allocTracker.add(new Coordinates(xCoordinate, yCoordinate));
+                allocations++;
+                }
+             }
+          }
         }
-    System.out.println(newGrid);
+    
+        
+    System.out.println(tilesAreNotFilled(newGrid));
+    System.out.println(Arrays.deepToString(newGrid));
     return newGrid;
 }
 
@@ -48,6 +80,130 @@ public class gameGenerator {
                 newGrid[x][y]= 0;
             }
         }
+
+    }
+
+    public static boolean checkForCompletion(int[][] newGrid) {
+        if (invalidsudoku(newGrid)) return false;
+        if (tilesAreNotFilled(newGrid)) return false;
+        return true;
+    }
+
+public static boolean invalidsudoku(int[][] newGrid){
+    if(rowsAreInvalid(newGrid)) return true;
+    if(columnsAreInvalid(newGrid)) return true;
+    if(squaresAreInvalid(newGrid)) return true;
+    else return false;
+
+
 }
+
+public static boolean columnsAreInvalid(int[][] newGrid){
+    for (int xIndex = 0; xIndex < 9; xIndex++){
+        List<Integer> row = new ArrayList<>();
+        for(int yIndex = 0; yIndex <9; yIndex++){
+            row.add(newGrid[xIndex][yIndex]);
+
+    }
+        if (collectionHasRepeats(row)) return true;
+}
+    return false;
+}
+public static boolean rowsAreInvalid(int[][] newGrid){
+    for (int yIndex = 0; yIndex < 9; yIndex++){
+        List<Integer> row = new ArrayList<>();
+        for(int xIndex = 0; xIndex <9; xIndex++){
+            row.add(newGrid[xIndex][yIndex]);
+
+    }
+        if (collectionHasRepeats(row)) return true;
+}
+    return false;
+
+}
+
+public static boolean collectionHasRepeats(List<Integer> collection) {
+    for (int index = 1; index <= 9; index++) {
+        if (Collections.frequency(collection, index) > 1) return true;
+    }
+
+    return false;
+}
+
+
+public static boolean squaresAreInvalid(int[][] grid) {
+    if (rowOfSquaresIsInvalid(Rows.TOP, grid)) return true;
+
+    if (rowOfSquaresIsInvalid(Rows.MIDDLE, grid)) return true;
+
+    if (rowOfSquaresIsInvalid(Rows.BOTTOM, grid)) return true;
+
+    return false;
+
+
+
+}
+
+private static boolean rowOfSquaresIsInvalid(Rows value, int[][] grid) {
+    switch (value) {
+        case TOP:
+            if (squareIsInvalid(0, 0, grid)) return true;
+            if (squareIsInvalid(0, 3, grid)) return true;
+            if (squareIsInvalid(0, 6, grid)) return true;
+
+            return false;
+
+        case MIDDLE:
+            if (squareIsInvalid(3, 0, grid)) return true;
+            if (squareIsInvalid(3, 3, grid)) return true;
+            if (squareIsInvalid(3, 6, grid)) return true;
+            return false;
+
+        case BOTTOM:
+            if (squareIsInvalid(6, 0, grid)) return true;
+            if (squareIsInvalid(6, 3, grid)) return true;
+            if (squareIsInvalid(6, 6, grid)) return true;
+            return false;
+
+        default:
+            return false;
+    }
+}
+
+public static boolean squareIsInvalid(int yIndex, int xIndex, int[][] grid) {
+    int yIndexEnd = yIndex + 3;
+    int xIndexEnd = xIndex + 3;
+
+    List<Integer> square = new ArrayList<>();
+
+    while (yIndex < yIndexEnd) {
+
+        while (xIndex < xIndexEnd) {
+            square.add(
+                    grid[xIndex][yIndex]
+            );
+            xIndex++;
+        }
+
+        //reset x to original value by subtracting by 2
+        xIndex -= 3;
+
+        yIndex++;
+    }
+
+    //if square has repeats, return true
+    if (collectionHasRepeats(square)) return true;
+    return false;
+}
+
+public static boolean tilesAreNotFilled(int[][] grid) {
+    for (int xIndex = 0; xIndex < 9; xIndex++) {
+        for (int yIndex = 0; yIndex < 9; yIndex++) {
+            if (grid[xIndex][yIndex] == 0) return true;
+        }
+    }
+    return false;
+}
+
 
 }
