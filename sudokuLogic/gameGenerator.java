360f7ea4b800b6a164d4b07dcf6656aad2503da3
LeeviOva <Leevi.ovaskainen@edu.turkuamk.fi>
2023-04-20 15:30:15 +0300

    Definitions for gameGenerator
    Added definitions into the gameGenerator.java, which explain how the program works.

--- sudokuLogic/gameGenerator.java
+++ sudokuLogic/gameGenerator.java
@@ -5,8 +5,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-import sudokuLogic.coordinates;
-import sudokuLogic.rows;
+import sudokuLogic.coordinates;     /*Coordinates for the solvedGameGeneration.*/
+import sudokuLogic.rows;            /*Rows for the checking of squares.*/
 
 
 
@@ -17,51 +17,50 @@ public class gameGenerator {
         Random random = new Random(System.currentTimeMillis());     /* Creating a random number generator */
         int[][] newGrid = new int[9][9];                            /* NOTE: this part is now usable for only 9 x 9 grids, smaller ones are off the table with this */
         
-        for (int value = 1; value <= 9; value++){                   /* Outer for loop for making sure dublicate numbers in the same positions wont occur. Tests all positions */
+        for (int value = 1; value <= 9; value++){                   /* Outer for loop for making sure dublicate numbers in the same positions wont occur. Tests all positions. */
             int allocations = 0;
             int interrupt = 0;
             int tries = 0;
-            List<coordinates> allocTracker = new ArrayList<>();
+            List<coordinates> allocTracker = new ArrayList<>(); /* Making an allocation tracker for backtracking if the generation doen't go smoothly (if the puzzle is unsolvable or if it includes zeroes.) */
 
             tries++;
 
-            while (allocations < 9){
-                if(interrupt > 200){
+            while (allocations < 9){                            /* Inner while loop for actually placing the "value" into the sudoku squares*/
+                if(interrupt > 200){                            /* If the program doesnt find a suitable number in the position it assign it as 0, so it can try again later. */
                     allocTracker.forEach(coord -> {
                         newGrid[coord.getX()][coord.getY()] = 0;   });
                         
                         allocations = 0;
                         interrupt = 0;
-                        allocTracker.clear();
-                         tries++;
+                        allocTracker.clear();                   /*In the case of and invalid puzzle it also clears the allocation tracker as it is backtracking to a prevoius state, which is stored in the allocation tracker. */
+                        tries++;                                /*Adds one to the "tries" variable which is used in the following if-loop */
                 
-                        if (tries > 500) {
+                        if (tries > 500) {                      /*When the "tries valuable is more than 500, the program is jammed and cannot find a suitable value for the position even after 500 tries, we delete the whole process and start from the beginning." */
                             nukeCurrentGame(newGrid);
                             tries = 0;
                             value = 1;
                 }
             }
-            int xCoordinate = random.nextInt(9);
+            int xCoordinate = random.nextInt(9);        /*Randomly chooses a position to assign the value to  */
             int yCoordinate = random.nextInt(9);
 
-            if(newGrid[xCoordinate][yCoordinate] == 0){
+            if(newGrid[xCoordinate][yCoordinate] == 0){     /* Assings the value*/
                 newGrid[xCoordinate][yCoordinate] = value;
                 
-                if(invalidsudoku(newGrid)) {
-                    newGrid[xCoordinate][yCoordinate] = 0;
-                    interrupt++;
+                if(invalidsudoku(newGrid)) {                /* Check if the sudoku is not valid so far without looking at the zeroes in the puzzle. */
+                    newGrid[xCoordinate][yCoordinate] = 0;  /*If not it sets the value as 0, meaning its an empty unassigned position and it tries again. */
+                    interrupt++;                            /* adds 1 to the interrupt - variable*/
             }
             else {
-                allocTracker.add(new coordinates(xCoordinate, yCoordinate));
-                allocations++;
+                allocTracker.add(new coordinates(xCoordinate, yCoordinate));       /* If all goes well it goes into the allocation tracker*/
+                allocations++;                                                     /*adds 1 to the allocations -variable */
                 }
              }
           }
         }
     
         
-    System.out.println(tilesAreNotFilled(newGrid));
-    System.out.println(Arrays.deepToString(newGrid));
+
     return newGrid;
 }
 
@@ -74,33 +73,33 @@ public class gameGenerator {
 
     }
 
-    public static boolean checkForCompletion(int[][] newGrid) {
+    public static boolean checkForCompletion(int[][] newGrid) {             /* This is used later on just before starting the game, where the program checks one last time just before unsolving it if its solvable and fully filled*/
         if (invalidsudoku(newGrid)) return false;
         if (tilesAreNotFilled(newGrid)) return false;
         return true;
     }
 
-public static boolean invalidsudoku(int[][] newGrid){
-    if(rowsAreInvalid(newGrid)) return true;
-    if(columnsAreInvalid(newGrid)) return true;
-    if(squaresAreInvalid(newGrid)) return true;
+public static boolean invalidsudoku(int[][] newGrid){       /* Checking if the sudoku isn't solvable*/
+    if(rowsAreInvalid(newGrid)) return true;                /* Checking if the rows of the puzzle arent solvable*/
+    if(columnsAreInvalid(newGrid)) return true;             /* Checking if the columns of the puzzle arent solvable*/
+    if(squaresAreInvalid(newGrid)) return true;             /* Checking if the squares of the puzzle arent solvable*/
     else return false;
 
 
 }
 
-public static boolean columnsAreInvalid(int[][] newGrid){
+public static boolean columnsAreInvalid(int[][] newGrid){   /* Checking if the columns are invalid by checking if the collection has repeating numbers*/
     for (int xIndex = 0; xIndex < 9; xIndex++){
         List<Integer> row = new ArrayList<>();
         for(int yIndex = 0; yIndex <9; yIndex++){
             row.add(newGrid[xIndex][yIndex]);
 
     }
-        if (collectionHasRepeats(row)) return true;
+        if (collectionHasRepeats(row)) return true;         /*  If not return false and go on with the generation*/
 }
     return false;
 }
-public static boolean rowsAreInvalid(int[][] newGrid){
+public static boolean rowsAreInvalid(int[][] newGrid){      /* Checking if the rows are invalid by checking if the collection has repeating numbers*/
     for (int yIndex = 0; yIndex < 9; yIndex++){
         List<Integer> row = new ArrayList<>();
         for(int xIndex = 0; xIndex <9; xIndex++){
@@ -109,21 +108,21 @@ public static boolean rowsAreInvalid(int[][] newGrid){
     }
         if (collectionHasRepeats(row)) return true;
 }
-    return false;
+    return false;                                           /*  If not return false and go on with the generation*/
 
 }
 
-public static boolean collectionHasRepeats(List<Integer> collection) {
-    for (int index = 1; index <= 9; index++) {
-        if (Collections.frequency(collection, index) > 1) return true;
+public static boolean collectionHasRepeats(List<Integer> collection) {     /*Method for checking if row, columns or squares have repeating numbers and calling them collections */
+    for (int index = 1; index <= 9; index++) {                              /* For loop which starts from the value of 1, because this way the program doesnt return true, if the collection has repeating zeroes / empty spaces*/
+        if (Collections.frequency(collection, index) > 1) return true;     /*We used the .frequency command as it's the fastest way to find frequently appearing numbers */
     }
 
     return false;
 }
 
 
-public static boolean squaresAreInvalid(int[][] grid) {
-    if (rowOfSquaresIsInvalid(rows.TOP, grid)) return true;
+public static boolean squaresAreInvalid(int[][] grid) {             /*The 'main' method that checks if the squares of the puzzle are invalid */
+    if (rowOfSquaresIsInvalid(rows.TOP, grid)) return true;         /*Includes all rows of squares seperatly*/
 
     if (rowOfSquaresIsInvalid(rows.MIDDLE, grid)) return true;
 
@@ -135,10 +134,10 @@ public static boolean squaresAreInvalid(int[][] grid) {
 
 }
 
-private static boolean rowOfSquaresIsInvalid(rows value, int[][] grid) {
+private static boolean rowOfSquaresIsInvalid(rows value, int[][] grid) {    /*A lower method which assings the starting values for the lowest method to check wheter the squares are invalid or not*/
     switch (value) {
         case TOP:
-            if (squareIsInvalid(0, 0, grid)) return true;
+            if (squareIsInvalid(0, 0, grid)) return true;   /*For example the first square starts from the coordinates [0][0] and uses this as a starting point in the following method.*/
             if (squareIsInvalid(0, 3, grid)) return true;
             if (squareIsInvalid(0, 6, grid)) return true;
 
@@ -157,44 +156,42 @@ private static boolean rowOfSquaresIsInvalid(rows value, int[][] grid) {
             return false;
 
         default:
-            return false;
+            return false;                                                    /*If it passes all the tests, returns false. */
     }
 }
 
-public static boolean squareIsInvalid(int yIndex, int xIndex, int[][] grid) {
-    int yIndexEnd = yIndex + 3;
+public static boolean squareIsInvalid(int yIndex, int xIndex, int[][] grid) { /*This method checks if the squares of the puzzle are invalid a row of 3 values at a time.*/
+    int yIndexEnd = yIndex + 3; 
     int xIndexEnd = xIndex + 3;
 
     List<Integer> square = new ArrayList<>();
 
     while (yIndex < yIndexEnd) {
 
-        while (xIndex < xIndexEnd) {
+        while (xIndex < xIndexEnd) {        /*Makes a row of 3 and adds it to the collection square*/
             square.add(
                     grid[xIndex][yIndex]
             );
-            xIndex++;
+            xIndex++;                       
         }
 
-        //reset x to original value by subtracting by 2
-        xIndex -= 3;
+        xIndex -= 3;                        /*After the row is added into the collection reset the x position.*/
 
-        yIndex++;
+        yIndex++;                           /*After reseting the x position add 1 into the y position so the the run in the while loop adds the second row of 3 below. */
     }
 
-    //if square has repeats, return true
-    if (collectionHasRepeats(square)) return true;
-    return false;
+    if (collectionHasRepeats(square)) return true;        
+    return false;                                           /*If all goes well return false*/
 }
 
-public static boolean tilesAreNotFilled(int[][] grid) {
+public static boolean tilesAreNotFilled(int[][] grid) {         /*Method to check if the puzzle contains any zeroes, so it returns true, if the tiles are not fully filled. */
     for (int xIndex = 0; xIndex < 9; xIndex++) {
         for (int yIndex = 0; yIndex < 9; yIndex++) {
             if (grid[xIndex][yIndex] == 0) return true;
         }
     }
-    return false;
-}
+    return false;                                               /*If the tiles are fully filled return false*/
+}   
 
 
 }
